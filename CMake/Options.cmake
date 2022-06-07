01298cbb07b592dbea67b6d9f463703ad945f630
yangzhg <yangzhgg@gmail.com>
2022-06-07 09:54:53 +0800

    add option to disable build test

--- CMake/Options.cmake
+++ CMake/Options.cmake
@@ -4,6 +4,7 @@ OPTION(ENABLE_SSE "enable SSE4.2 buildin function" ON)
 OPTION(ENABLE_FRAME_POINTER "enable frame pointer on 64bit system with flag -fno-omit-frame-pointer, on 32bit system, it is always enabled" ON)
 OPTION(ENABLE_LIBCPP "using libc++ instead of libstdc++, only valid for clang compiler" OFF)
 OPTION(ENABLE_BOOST "using boost instead of native compiler c++0x support" OFF)
+OPTION(BUILD_TEST "build test files" OFF)
 
 INCLUDE (CheckFunctionExists)
 CHECK_FUNCTION_EXISTS(dladdr HAVE_DLADDR)
