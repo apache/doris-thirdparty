e8a1558f1a132cc730103fdde048dc6498d8f149
Adonis Ling <adonis0147@gmail.com>
2023-03-28 17:38:44 +0800

    [chore] Porting to Clang-16

--- cmake/TurboPFOR.cmake
+++ cmake/TurboPFOR.cmake
@@ -2,7 +2,7 @@ PROJECT(turbo-pfor)
 set(PFOR_SOURCE_DIR ${CMAKE_SOURCE_DIR}/src/ext/for)
 
 add_custom_target(build_ic ALL
-	COMMAND make USE_AVX2=${USE_AVX2} libic.a -j 8
+	COMMAND make OPT='-Wno-implicit-function-declaration -Wno-implicit-int -Wno-int-conversion' USE_AVX2=${USE_AVX2} libic.a -j 8
         WORKING_DIRECTORY ${PFOR_SOURCE_DIR}
         COMMENT "Original Turbo-PFOR makefile target")
 
