e8786e667152989b6386acb9d8102a3662e23a18
姜凯 <airborne08@gmail.com>
2023-05-09 09:34:19 +0800

    [Chore] add internal roaring bitmap

--- cmake/FindRoaring.cmake
+++ cmake/FindRoaring.cmake
@@ -13,9 +13,18 @@ find_package_handle_standard_args(Roaring
   REQUIRED_VARS Roaring_INCLUDE_DIR Roaring_LIBRARY
 )
 
-if(Roaring_FOUND)
-  set(Roaring_INCLUDE_DIRS ${Roaring_INCLUDE_DIR})
-  set(Roaring_LIBRARIES ${Roaring_LIBRARY})
-  mark_as_advanced(Roaring_INCLUDE_DIR Roaring_LIBRARY)
-endif()
+if(NOT Roaring_FOUND)
+  message(STATUS "Roaring not found, trying to build from ext directory.")
 
+  set(EXT_ROARING_DIR "${clucene-ext_SOURCE_DIR}/roaring")
+  if(EXISTS "${EXT_ROARING_DIR}/roaring.c" AND EXISTS "${EXT_ROARING_DIR}/roaring.h" AND EXISTS "${EXT_ROARING_DIR}/roaring.hh")
+    set(Roaring_INCLUDE_DIR ${EXT_ROARING_DIR})
+    add_library(roaring STATIC "${EXT_ROARING_DIR}/roaring.c")
+    #target_include_directories(roaring INTERFACE ${Roaring_INCLUDE_DIR})
+    set(Roaring_LIBRARY roaring)
+    set(Roaring_FOUND TRUE)
+    message(STATUS "Roaring will be built from ext directory.")
+  else()
+    message(FATAL_ERROR "Roaring not found and ext directory does not contain required files.")
+  endif()
+endif()
