bbfc6561351a0e96f44609aad5ee008be201bd28
Adonis Ling <adonis0147@gmail.com>
2023-05-30 22:06:50 +0800

    [chore] Fix the workflow (#84)
    Fix lots of issues with GitHub workflows.

--- build.sh
+++ build.sh
@@ -29,34 +29,34 @@ export SRC_HOME="${ROOT}"
 . "${SRC_HOME}/env.sh"
 
 if [[ -z "${THIRDPARTY_INSTALLED}" ]]; then
-    echo "Must set 'THIRDPARTY_INSTALLED' in env.sh"
-    exit -1
+	echo "Must set 'THIRDPARTY_INSTALLED' in env.sh"
+	exit 255
 fi
 
-DIST_DIR=${SRC_HOME}/hadoop-dist/target/hadoop-3.3.4/
-LIBHDFS_DIST_DIR=${SRC_HOME}/hadoop-dist/target/hadoop-libhdfs-3.3.4/
-rm -rf ${DIST_DIR}
-rm -rf ${LIBHDFS_DIST_DIR}
+DIST_DIR="${SRC_HOME}/hadoop-dist/target/hadoop-3.3.6"
+LIBHDFS_DIST_DIR="${SRC_HOME}/hadoop-dist/target/hadoop-libhdfs-3.3.6"
+rm -rf "${DIST_DIR}"
+rm -rf "${LIBHDFS_DIST_DIR}"
 
 export PATH=${THIRDPARTY_INSTALLED}/bin:$PATH
-mvn clean package -Pnative,dist -DskipTests -Dmaven.javadoc.skip=true -Dthirdparty.installed=${THIRDPARTY_INSTALLED}/ -Dopenssl.lib=${THIRDPARTY_INSTALLED}/lib/ -e
+mvn clean package -Pnative,dist -DskipTests -Dmaven.javadoc.skip=true \
+	-Dthirdparty.installed="${THIRDPARTY_INSTALLED}" -Dopenssl.prefix="${THIRDPARTY_INSTALLED}" -e
 
 if [[ ! -d "${DIST_DIR}" ]]; then
-    echo "${DIST_DIR} is missing. Build failed."
-    exit -1
+	echo "${DIST_DIR} is missing. Build failed."
+	exit 255
 fi
 
 echo "Finished. Begin to pacakge for libhdfs..."
-mkdir -p ${LIBHDFS_DIST_DIR}/common
-mkdir -p ${LIBHDFS_DIST_DIR}/hdfs
-mkdir -p ${LIBHDFS_DIST_DIR}/include
-mkdir -p ${LIBHDFS_DIST_DIR}/native
-cp -r ${DIST_DIR}/share/hadoop/common/* ${LIBHDFS_DIST_DIR}/common/
-cp -r ${DIST_DIR}/share/hadoop/hdfs/* ${LIBHDFS_DIST_DIR}/hdfs/
-cp -r ${DIST_DIR}/include/hdfs.h ${LIBHDFS_DIST_DIR}/include/
-cp -r ${DIST_DIR}/lib/native/libhdfs.a ${LIBHDFS_DIST_DIR}/native/
+mkdir -p "${LIBHDFS_DIST_DIR}/common"
+mkdir -p "${LIBHDFS_DIST_DIR}/hdfs"
+mkdir -p "${LIBHDFS_DIST_DIR}/include"
+mkdir -p "${LIBHDFS_DIST_DIR}/native"
+cp -r "${DIST_DIR}/share/hadoop/common"/* "${LIBHDFS_DIST_DIR}/common"/
+cp -r "${DIST_DIR}/share/hadoop/hdfs"/* "${LIBHDFS_DIST_DIR}/hdfs"/
+cp -r "${DIST_DIR}/include/hdfs.h" "${LIBHDFS_DIST_DIR}/include"/
+cp -r "${DIST_DIR}/lib/native/libhdfs.a" "${LIBHDFS_DIST_DIR}/native"/
 
 echo "Done!"
 echo "The full dist package is under: ${DIST_DIR}"
 echo "The LIBHDFS dist package is under: ${LIBHDFS_DIST_DIR}"
-
