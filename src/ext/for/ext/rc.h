e225a9da29284a917f01dd086eafbae2de8d22b1
airborne12 <airborne12@gmail.com>
2023-06-11 19:30:50 +0800

    [Fix](PFOR) revert TurboPFOR to last version, and fix some build issue

--- src/ext/for/ext/rc.h
+++
@@ -1,8 +0,0 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-unsigned char *rcenc32(unsigned* input, int size, unsigned* output);
-unsigned char *rcdec32(unsigned* input, int size, unsigned* output);
-#ifdef __cplusplus
-}
-#endif
