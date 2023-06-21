103e88a8a3b24da9ae2a0d9908a3ceb3f7808a61
zzzxl <33418555+zzzxl1993@users.noreply.github.com>
2023-06-21 11:06:44 +0800

    [fix](jiebafix ) cut word greater than 225 heap-use-after-free (#96)

--- src/contribs-lib/CLucene/analysis/jieba/ChineseTokenizer.h
+++ src/contribs-lib/CLucene/analysis/jieba/ChineseTokenizer.h
@@ -44,7 +44,7 @@ private:
      * character buffer, store the characters which are used to compose <br>
      * the returned Token
      */
-    TCHAR buffer[LUCENE_MAX_WORD_LEN]{};
+    TCHAR buffer[LUCENE_MAX_WORD_LEN + 1]{};
 
     /**
      * I/O buffer, used to store the content of the input(one of the <br>
