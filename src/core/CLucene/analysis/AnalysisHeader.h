34c2c6712e27a8c94e9cdb426a0a74ad4bd7d7e5
airborne12 <airborne08@gmail.com>
2023-07-11 17:59:35 +0800

    [Compile] fix compile problem (#99)

--- src/core/CLucene/analysis/AnalysisHeader.h
+++ src/core/CLucene/analysis/AnalysisHeader.h
@@ -10,7 +10,6 @@
 #include "CLucene/index/Payload.h"
 #include "CLucene/util/VoidList.h"
 #include "CLucene/LuceneThreads.h"
-#include "CLucene/config/repl_tchar.h"
 
 CL_CLASS_DEF(util,Reader)
 CL_CLASS_DEF(util,IReader)
@@ -227,7 +226,7 @@ inline size_t Token::termLength<char>(){
 template <>
 inline size_t Token::termLength<TCHAR>(){
     if ( _termTextLen == -1 ) //it was invalidated by growBuffer
-        _termTextLen = _tcslen((TCHAR*)_buffer);
+        _termTextLen = wcslen((TCHAR*)_buffer);
     return _termTextLen;
 };
 
