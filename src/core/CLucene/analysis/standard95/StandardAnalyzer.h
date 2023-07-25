454cdd2c99e8a093cea3a434eabafbe7f5d55302
zzzxl <33418555+zzzxl1993@users.noreply.github.com>
2023-07-25 20:40:36 +0800

    [improvement](invert index) chinese word cutall does not cut english and numbers (#104)

--- src/core/CLucene/analysis/standard95/StandardAnalyzer.h
+++ src/core/CLucene/analysis/standard95/StandardAnalyzer.h
@@ -8,13 +8,13 @@ class StandardAnalyzer : public Analyzer {
  public:
   TokenStream* tokenStream(const TCHAR* fieldName,
                            lucene::util::Reader* reader) override {
-    return _CLNEW StandardTokenizer(reader);
+    return _CLNEW StandardTokenizer(reader, useStopWords_);
   }
 
   TokenStream* reusableTokenStream(const TCHAR* fieldName,
                                    lucene::util::Reader* reader) override {
     if (tokenizer_ == nullptr) {
-      tokenizer_ = new StandardTokenizer(reader);
+      tokenizer_ = new StandardTokenizer(reader, useStopWords_);
     } else {
       tokenizer_->reset(reader);
     }
@@ -28,7 +28,13 @@ class StandardAnalyzer : public Analyzer {
     }
   }
 
+  void useStopWords(bool useStopWords) {
+    useStopWords_ = useStopWords;
+  }
+
  private:
+  bool useStopWords_ = true;
+
   StandardTokenizer* tokenizer_ = nullptr;
 };
 
