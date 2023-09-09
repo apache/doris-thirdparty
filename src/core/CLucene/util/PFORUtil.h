c0569da1c504d0dd871cb45ff52d7226a6b60d65
姜凯 <airborne08@gmail.com>
2023-09-09 10:09:44 +0800

    [Fix](PFOR) fix build error in arm

--- src/core/CLucene/util/PFORUtil.h
+++ src/core/CLucene/util/PFORUtil.h
@@ -18,7 +18,6 @@
 
 #include <cstddef>
 #include <cstdint>
-#include <cpuid.h>
 
 size_t P4DEC(unsigned char *__restrict in, size_t n, uint32_t *__restrict out);
 size_t P4NZDEC(unsigned char *__restrict in, size_t n, uint32_t *__restrict out);
