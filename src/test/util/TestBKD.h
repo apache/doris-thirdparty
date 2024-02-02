9ad844199fb3fcd1857d0e706742806863785177
姜凯 <airborne08@gmail.com>
2024-02-02 17:49:27 +0800

    [Fix](test) fix unitest error

--- src/test/util/TestBKD.h
+++ src/test/util/TestBKD.h
@@ -26,20 +26,19 @@ public:
         }
     }
     void visit(std::vector<char>& docID, std::vector<uint8_t> &packedValue) override {
-        if (matches(packedValue.data()) != 0) {
+        if (!matches(packedValue.data())) {
             return;
         }
         visit(roaring::Roaring::read(docID.data(), false));
     }
     void visit(roaring::Roaring *docID, std::vector<uint8_t> &packedValue) override;
+    void visit(int docID, std::vector<uint8_t> &packedValue) override;
     void visit(lucene::util::bkd::bkd_docid_set_iterator *iter, std::vector<uint8_t> &packedValue) override;
-    int visit(int docid, std::vector<uint8_t> &packedValue) override;
 
-    int matches(uint8_t *packedValue);
+    bool matches(uint8_t *packedValue);
 
     lucene::util::bkd::relation compare(std::vector<uint8_t> &minPacked,
                                         std::vector<uint8_t> &maxPacked) override;
-    lucene::util::bkd::relation compare_prefix(std::vector<uint8_t> &prefix) override;
 };
 
 enum predicate {
@@ -50,17 +49,20 @@ enum predicate {
     EQ
 };
 
-template <predicate QT>
 class TestVisitor : public lucene::util::bkd::bkd_reader::intersect_visitor {
 private:
     const uint8_t *queryMin;
     const uint8_t *queryMax;
+    //int queryMin = 0;
+    //int queryMax = 0;
     lucene::util::BitSet *hits;
+    //std::shared_ptr<lucene::util::BitSet> hits;
     std::shared_ptr<lucene::util::bkd::bkd_reader> reader;
+    predicate pred;
 
 public:
-    TestVisitor(const uint8_t *queryMin, const uint8_t *queryMax, lucene::util::BitSet *hits);
-    ~TestVisitor() override = default;
+    TestVisitor(const uint8_t *queryMin, const uint8_t *queryMax, lucene::util::BitSet *hits, predicate p);
+    virtual ~TestVisitor() = default;
 
     void setReader(std::shared_ptr<lucene::util::bkd::bkd_reader> &r) { reader = r; };
 
@@ -99,10 +101,9 @@ public:
             docID = iter->docid_set->nextDoc();
         }
     };
-    int matches(uint8_t *packedValue);
-    lucene::util::bkd::relation compare_prefix(std::vector<uint8_t> &prefix) override;
+    bool matches(uint8_t *packedValue);
 
-    int visit(int rowID, std::vector<uint8_t> &packedValue) override;
+    void visit(int rowID, std::vector<uint8_t> &packedValue) override;
 
     lucene::util::bkd::relation compare(std::vector<uint8_t> &minPacked,
                                         std::vector<uint8_t> &maxPacked) override;
