96ff57c2c829d58077c46f71b4a0bd8cb724754f
zzzxl1993 <yangsiyu@selectdb.com>
2025-02-19 16:19:21 +0800

    [opt](inverted index) Refactor ICU tokenizer code location for better organization and maintainability.

--- src/test/tests.cpp
+++ src/test/tests.cpp
@@ -23,7 +23,6 @@ unittest tests[] = {
         {"IndexCompressV3", testIndexCompressV3},
         {"ByteArrayDataInput", testByteArrayDataInputSuite},
         {"GrowableByteArrayDataOutput", testGrowableByteArrayDataOutputSuite},
-        {"testICU", testICU},
 #ifdef TEST_CONTRIB_LIBS
         {"chinese", testchinese},
 #endif
