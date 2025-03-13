1b0c0f00e9448a4e2866cc3449b02cc4d727abf7
Ruiyang Wu <ywwry66@outlook.com>
2025-03-13 02:25:52 -0400

    CMake: Avoid mixed OpenMP linkage

--- cmake/system.cmake
+++ cmake/system.cmake
@@ -377,6 +377,12 @@ if (USE_OPENMP)
   set(CCOMMON_OPT "${CCOMMON_OPT} -DUSE_OPENMP")
   if (NOT NOFORTRAN)
     find_package(OpenMP COMPONENTS Fortran REQUIRED)
+    # Avoid mixed OpenMP linkage
+    get_target_property(OMP_C_LIB OpenMP::OpenMP_C INTERFACE_LINK_LIBRARIES)
+    get_target_property(OMP_Fortran_LIB OpenMP::OpenMP_Fortran INTERFACE_LINK_LIBRARIES)
+    if (NOT OMP_C_LIB STREQUAL OMP_Fortran_LIB)
+      message(FATAL_ERROR "Multiple OpenMP runtime libraries detected. Mixed OpenMP runtime linkage is dangerous. You may pass -DOpenMP_LANG_LIB_NAMES and -DOpenMP_omp_LIBRARY to manually choose the OpenMP library.")
+    endif()
   endif ()
 endif ()
 
