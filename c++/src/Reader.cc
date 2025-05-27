38199fffb0c198bc6e385edc6144ea9762c89411
Qi Chen <chenqi@selectdb.com>
2025-05-27 17:14:55 +0800

    [fix] Load StripeIndex if searchArgument not exist when filter is not null. (#318)

--- c++/src/Reader.cc
+++ c++/src/Reader.cc
@@ -1279,6 +1279,11 @@ namespace orc {
             currentRowInStripe = 0;
             continue;
           }
+        } else {
+          if (filter) {
+            // read row group statistics and bloom filters of current stripe
+            loadStripeIndex();
+          }
         }
       }
 
