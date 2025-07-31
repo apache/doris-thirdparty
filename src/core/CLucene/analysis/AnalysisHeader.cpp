cd23cf98323a05dc24799901185e8c6669a5855f
姜凯 <jiangkai@selectdb.com>
2025-07-31 18:45:22 +0800

    [opt] remove useless lock in analyzer

--- src/core/CLucene/analysis/AnalysisHeader.cpp
+++ src/core/CLucene/analysis/AnalysisHeader.cpp
@@ -13,25 +13,9 @@
 CL_NS_USE(util)
 CL_NS_DEF(analysis)
 
-struct Analyzer::Internal{
-    CL_NS(util)::ThreadLocal<TokenStream*,
-                             CL_NS(util)::Deletor::Object<TokenStream> >* tokenStreams;
-};
-Analyzer::Analyzer(){
-    _internal = new Internal;
-    _internal->tokenStreams = _CLNEW CL_NS(util)::ThreadLocal<TokenStream*,
-                                                              CL_NS(util)::Deletor::Object<TokenStream> >;
-}
-Analyzer::~Analyzer(){
-    _CLLDELETE(_internal->tokenStreams);
-    delete _internal;
-}
-TokenStream* Analyzer::getPreviousTokenStream() {
-    return _internal->tokenStreams->get();
-}
-void Analyzer::setPreviousTokenStream(TokenStream* obj) {
-    _internal->tokenStreams->set(obj);
-}
+Analyzer::Analyzer(){}
+Analyzer::~Analyzer(){}
+
 TokenStream* Analyzer::reusableTokenStream(const TCHAR* fieldName, CL_NS(util)::Reader* reader) {
     return tokenStream(fieldName, reader);
 }
